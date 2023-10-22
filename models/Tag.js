cd69ee6dd740deeb390452aafca756f9e67535d9
JayBassCodes <jacob.m.bassett@gmail.com>
2023-10-22 14:09:37 -0400

    added models' script

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,24 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true
+    },
+    name:
+    {
+      // String
+      type: DataTypes.STRING,
+      // Doesn't allow null values
+      allowNull: false
+    }
+
   },
   {
     sequelize,
